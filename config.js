a496e645ba64b57c78c4fa2a77311c1ec03ff741
scheurermarc77-spec <scheurermarc77@gmail.com>
2026-08-30 22:23:46 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Diese Datei wird ab Version 16 nicht mehr von der App geladen.
-// Die Verbindung ist direkt in index.html/app.js hinterlegt.
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://kfpxheegmeupnuzqjqqt.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_vlP2dIHDTK-VY5LK-jeS_w_tN04WaK0",
-  HOUSEHOLD_ID: "leon-papi-mami-anouk"
-};
